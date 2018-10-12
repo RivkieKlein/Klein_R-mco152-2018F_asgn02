b26b4820a55753dd7966e15a1b8b2263f4933fc4
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2018-10-12 01:16:13 -0400

    Hello World changes to Adder

--- src/asgn02/HelloWorld.java
+++ src/asgn02/HelloWorld.java
@@ -1,9 +1,16 @@
 package asgn02;
-
+import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String []args) {
-		System.out.println("Hello World");
-		
+	Scanner keyboard = new Scanner(System.in);
+	
+	System.out.println("Enter a number");
+	int num1=keyboard.nextInt();
+	
+	System.out.println("Enter another number");
+	int num2=keyboard.nextInt();
+	
+	System.out.println(num1+" +76 "+num2+" = "+(num1+num2));
 	}
 
 }
