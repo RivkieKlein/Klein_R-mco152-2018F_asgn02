5c7daac0eafb7de67e5910d527b77275760ec512
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2018-10-12 01:45:12 -0400

    Subtractor class created and HelloWorld now also subtracts

--- src/asgn02/HelloWorld.java
+++ src/asgn02/HelloWorld.java
@@ -5,12 +5,14 @@ public class HelloWorld {
 	Scanner keyboard = new Scanner(System.in);
 	
 	System.out.println("Enter 2 numbers separated by a space");
+	int num1 =keyboard.nextInt();
+	int num2 = keyboard.nextInt();
 	
-	
-	Adder adder = new Adder(keyboard.nextInt(), keyboard.nextInt());
+	Adder adder = new Adder(num1, num2);
+	Subtractor subber = new Subtractor(num1, num2);
 	
 	System.out.println( adder.getnum1()+" + "+adder.getnum2()+ " = "+adder.add());
-	
+	System.out.println( subber.getnum1()+" - "+subber.getnum2()+ " = "+subber.subtract());
 	keyboard.close();
 	}
 
