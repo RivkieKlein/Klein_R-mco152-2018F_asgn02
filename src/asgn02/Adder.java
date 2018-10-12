70d5dfc977a98db839fb48de26b8a75124877e25
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2018-10-12 01:36:45 -0400

    Adder class created and HelloWorld now adds using Adder class

---
+++ src/asgn02/Adder.java
@@ -0,0 +1,25 @@
+package asgn02;
+
+public class Adder {
+	private int num1;
+	private int num2;
+	
+	public Adder(int num1, int num2) {
+		this.num1=num1;
+		this.num2=num2;
+				
+	}
+	
+	public int add() {
+		return(num1+num2);
+	}
+	
+	public int getnum1() {
+		return num1;
+	}
+	
+	public int getnum2() {
+		return num2;
+	}
+
+}
--- src/asgn02/HelloWorld.java
+++ src/asgn02/HelloWorld.java
@@ -4,13 +4,14 @@ public class HelloWorld {
 	public static void main(String []args) {
 	Scanner keyboard = new Scanner(System.in);
 	
-	System.out.println("Enter a number");
-	int num1=keyboard.nextInt();
+	System.out.println("Enter 2 numbers separated by a space");
 	
-	System.out.println("Enter another number");
-	int num2=keyboard.nextInt();
 	
-	System.out.println(num1+" +76 "+num2+" = "+(num1+num2));
+	Adder adder = new Adder(keyboard.nextInt(), keyboard.nextInt());
+	
+	System.out.println( adder.getnum1()+" + "+adder.getnum2()+ " = "+adder.add());
+	
+	keyboard.close();
 	}
 
 }
