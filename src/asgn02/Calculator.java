78e6457b9c01a287af830f835ff5d6c79a467e09
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2018-10-12 02:03:35 -0400

    Calculator replaces both Adder and Subtractor classes. Hello World now uses Calculator class

--- src/asgn02/Calculator.java
+++ src/asgn02/Calculator.java
@@ -1,13 +1,17 @@
 package asgn02;
 
-public class Adder {
+public class Calculator {
 	private int num1;
 	private int num2;
 	
-	public Adder(int num1, int num2) {
+	public Calculator(int num1, int num2) {
 		this.num1=num1;
 		this.num2=num2;
 				
+	}
+	
+	public int subtract() {
+		return(num1-num2);
 	}
 	
 	public int add() {
@@ -21,5 +25,7 @@ public class Adder {
 	public int getnum2() {
 		return num2;
 	}
-
 }
+
+
+
--- src/asgn02/HelloWorld.java
+++ src/asgn02/HelloWorld.java
@@ -3,17 +3,34 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String []args) {
 	Scanner keyboard = new Scanner(System.in);
+	char cont;
 	
+	do {
+	//get user input	
 	System.out.println("Enter 2 numbers separated by a space");
 	int num1 =keyboard.nextInt();
 	int num2 = keyboard.nextInt();
+	keyboard.nextLine();
 	
-	Adder adder = new Adder(num1, num2);
-	Subtractor subber = new Subtractor(num1, num2);
+	//calculator object
+	Calculator calc = new Calculator(num1, num2);
 	
-	System.out.println( adder.getnum1()+" + "+adder.getnum2()+ " = "+adder.add());
-	System.out.println( subber.getnum1()+" - "+subber.getnum2()+ " = "+subber.subtract());
-	keyboard.close();
+	//user choosed action
+	System.out.println("Would you like to add or subtract these 2 numbers? a or s");
+	char choice = keyboard.nextLine().toUpperCase().charAt(0);
+	
+	if(choice=='A') {
+	System.out.println( calc.getnum1()+" + "+calc.getnum2()+ " = "+calc.add());
+	}
+	
+	else if(choice =='S') {
+	System.out.println( calc.getnum1()+" - "+calc.getnum2()+ " = "+calc.subtract());
+	}
+	
+	System.out.println("Would you like to continue adding and subtracting?y or n");
+	cont = keyboard.nextLine().toUpperCase().charAt(0);
+	
+	}while(cont != 'N');
 	}
 
 }
